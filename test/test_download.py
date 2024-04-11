f93947ff69e864d3de1514fec47f1fba41ec6ff5
armkhudinyan <armkhudinyan@gmail.com>
2024-04-11 12:33:37 +0100

    Fixes small data download for unit test

--- test/test_download.py
+++ test/test_download.py
@@ -16,6 +16,10 @@ class TestDownload:
     password = environ.get("OPENEO_PASS")
     footprint = 'POLYGON((40 -20, 40 -15, 30 -15, 30 -20, 40 -20))'
     filters = {'orbitDirection': ['ASCENDING']}
+    small_prods = pd.DataFrame([
+        {'Id': '0ca0cc0f-944b-4647-81d2-93694a2bf5aa', 'Name': 'S1A_WV_OCN.SAFE'},
+        {'Id': '3d65e0e5-016d-403f-a31a-227737d214a8', 'Name': 'S1A_WV_OCN.SAFE'}
+    ])
 
     def api_instance(self):
         # Initialize the API instance
@@ -62,11 +66,10 @@ class TestDownload:
     def test_download_all(self):
         # download test files in temporary output directory
         with tempfile.TemporaryDirectory() as tmp_dir:
-            # Query the products
-            products = self.products()
-            # Perform download for just 2 samples
-            self.api_instance().download_all(products[:2], Path(tmp_dir))
+            # Perform download for just 2 small size samples
+            dir = Path(tmp_dir)
+            self.api_instance().download_all(self.small_prods, dir)
 
             # Add assertions to check if the files are downloaded properly
-            assert (Path(tmp_dir) / f"{products.iloc[0]['Name']}.zip").exists()
-            assert (Path(tmp_dir) / f"{products.iloc[1]['Name']}.zip").exists()
+            assert (dir / f"{self.small_prods.iloc[0]['Name']}.zip").exists()
+            assert (dir / f"{self.small_prods.iloc[1]['Name']}.zip").exists()
